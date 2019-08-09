89b6636fb326f4ec3473509cebf1a767f3e36de4
Lili <lji@lingoport.com>
2019-08-09 11:50:43 -0600

    Update OverviewLicense.js

--- src/main/js/components/OverviewLicense.js
+++ src/main/js/components/OverviewLicense.js
@@ -9,9 +9,19 @@ import '../style.css';
 
 export default class OverviewLicense extends React.PureComponent {
 
+  function DateDifference(Date1,Date2) { //Date1和Date2是2017-07-10格式
+    var sDate, newDate1, newDate2, Days
+    aDate = Date1.split("-");
+    newDate1 = new Date(aDate[1] + '-' + aDate[2] + '-' + aDate[0]); //转换为07-10-2017格式
+    aDate = Date2.split("-");
+    newDate2 = new Date(aDate[1] + '-' + aDate[2] + '-' + aDate[0]);
+    Days = parseInt((newDate1 - newDate2) / 1000 / 60 / 60 / 24); //把差的毫秒数转换为天数
+    return Days;
+}
+
   render() {
     var gyzrExist = this.props.measure.gyzrExists
-
+    var current = new Date();
     if(gyzrExist===undefined&&this.props.measure.lrmExists===undefined){
       return (
         <div className="block" id="block_1">
@@ -29,7 +39,14 @@ export default class OverviewLicense extends React.PureComponent {
 
       );
     }else if(gyzrExist===undefined){
-
+    var lrmDate = this.props.measure.lrmEndDate;
+    var diff = DateDifference(current,lrmDate);
+    var lrm_diff;
+    if(Number(diff)>0){
+      lrm_diff = (
+        <td>LRM License End Date (expired):		</td>
+      );
+    }
     return (
       <div className="block" id="block_1">
       <div className="lplicensewidget" style={{height:'100%'}}>
@@ -41,6 +58,7 @@ export default class OverviewLicense extends React.PureComponent {
       <tbody>
         <tr>
           <td>LRM License End Date:		</td>
+          {lrm_diff}
           <td>{this.props.measure.lrmEndDate}</td>
         </tr>
         <tr>
