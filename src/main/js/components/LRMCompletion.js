c7fda53e1376626528013ba34f04860eae768655
Lili <lji@lingoport.com>
2019-08-29 10:57:18 -0600

    Update LRMCompletion.js

--- src/main/js/components/LRMCompletion.js
+++ src/main/js/components/LRMCompletion.js
@@ -62,7 +62,7 @@ export default class LRMCompletion extends React.PureComponent {
            <td className="bar" valign="middle">
 
            <div style={{width: '100%', height: '17px', backgroundColor: '#DDDDDD'}}>
-           <div style={{width:p, height:'11px',lineHeight:'11px',backgroundColor: '#EAE9FF',borderTop:'3px solid #DDDDDD',color:'#BBBBBB',fontSize:'12px'}}>{percent[d]}%</div>
+           <div style={{width:p, height:'11px',lineHeight:'11px',backgroundColor: '#EAE9FF',borderTop:'3px solid #DDDDDD',color:'#0a0000',fontSize:'12px'}}>{percent[d]}%</div>
            </div>
           </td>
            <td>{tfiles[d].substring(0,tfiles[d].length-2)}</td>
